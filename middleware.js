ae86630e903abec44b81823481fb9002bdc70035
codewithrabeeh <real.ray48@gmail.com>
2022-06-21 10:43:47 +0530

    added currentUser Helper

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log('REQ.USER...', req.user)
     if(!req.isAuthenticated()){
         req.flash('error', 'You Must Be Signed In')
         return res.redirect('/login')
